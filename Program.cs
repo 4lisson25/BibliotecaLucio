9515a94f479778fc74f2b5619b95587432081593
4lisson25 <alissondosv@gmail.com>
2025-03-10 14:53:19 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@
 // O usuário deve poder cadastrar quantos leitores ou livros ele desejar
 
 /* Alunos:
-    - Alisson Rafael da Cruz Velho
+    - Alisson Rafael da Cruz Velho (testando, testando, testando)
     - Marcos Vinicius Arruda Vandresen
     - Rodrigo Vaisam Bastos
     - Vinicius de Liz da Conceição
